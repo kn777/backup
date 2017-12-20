1ae336c7cdedf9f5ebd41ac3f2130f298edbc9f0
kn777 <kn_777@docomo.ne.jp>
2017-12-20 16:37:09 +0900

    12/20

---
+++ Person/src/Test.java
@@ -0,0 +1,33 @@
+/**
+ *
+ */
+
+/**
+ * @author internousdev
+ *
+ */
+public class Test {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+
+	Person kana = new Person();
+	kana.name ="岡香奈";
+	kana.age =20;
+	kana.phonenumber ="08088888888";
+	kana.address ="埼玉県飯能市";
+	System.out.println(kana.name);
+	System.out.println(kana.age);
+	System.out.println(kana.phonenumber);
+	System.out.println(kana.address);
+    kana.talk();
+
+
+
+}
+	}
+
+
